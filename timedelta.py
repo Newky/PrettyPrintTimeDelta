0bc1fcf8a4f462e8adbde265c6f65c096d67b175
Newky <richdel1991@gmail.com>
2013-06-12 22:24:08 +0100

    Simple class which prints out a nice years, months and day representation.

---
+++ timedelta.py
@@ -0,0 +1,154 @@
+MONTHS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+class ImprovedTimeDelta():
+    def __init__(self, from_date, to_date):
+        if to_date < from_date:
+            raise Exception("to_date should be later than from_date")
+        self.to_date = to_date
+        self.from_date = from_date
+        self.diff = to_date - from_date
+
+    def _is_leap_year(self, year):
+        if year % 400 == 0:
+            return True
+        elif year % 100 == 0:
+            return False
+        if year % 4 == 0:
+            return True
+        return False
+
+    def __str__(self):
+        d = self.days()
+        months, days = self._get_months(d)
+        years = self._get_years()
+        return "%d Years, %d Months, %d Days" % (years, months, days)
+
+    def add_on_if_leap(self, month_day_count, date_obj):
+        if self._is_leap_year(date_obj.year):
+            if date_obj.month == 2:
+                return month_day_count + 1
+        return month_day_count
+
+    def _get_months(self, d):
+
+        if self.from_date.month == self.to_date.month:
+            if self.from_date.day == self.to_date.day:
+                return 0, 0
+            if self.to_date.day > self.from_date.day:
+                return 0, self.to_date.day - self.from_date.day
+
+        if self.from_date.day == self.to_date.day:
+            if self.from_date.month > self.to_date.month:
+                return ((self.to_date.month + 12) - (self.from_date.month)), 0
+            else:
+                return (self.to_date.month - self.from_date.month), 0
+        start_month_day_count = MONTHS[self.from_date.month - 1]
+        start_month_day_count = self.add_on_if_leap(start_month_day_count,
+                self.from_date)
+
+        start_date = self.from_date.day
+        # days at start, plus 1 to include the current day
+        remaining_days = (start_month_day_count - start_date + 1)
+        end_date = self.to_date.day
+        remaining_days += end_date
+
+        month_count = 0
+        month_iterator = self.from_date.month + 1
+        while self.month_stop(month_iterator):
+            month_count += 1
+            month_iterator += 1
+            if month_iterator == 12:
+                month_iterator = 1
+
+        return month_count, remaining_days
+
+    def month_stop(self, month_iterator):
+        if month_iterator == self.to_date.month:
+            return False
+        if month_iterator == self.from_date.month:
+            return False
+        return True
+
+    def days(self):
+        return self.diff.days
+
+    def _get_years(self):
+        years = (self.to_date.year - self.from_date.year)
+        if self.to_date.month > self.from_date.month:
+            return years
+        elif self.to_date.month < self.from_date.month:
+            return years - 1
+        else:
+            if self.to_date.day < self.from_date.day:
+                return years - 1
+            else:
+                return years
+
+if __name__ == "__main__":
+    from datetime import datetime
+
+    x = datetime(2012, 01, 01)
+    y = datetime(2012, 01, 02)
+    td = ImprovedTimeDelta(x, y)
+    print td
+    assert str(td) == "0 Years, 0 Months, 1 Days"
+
+    x = datetime(2012, 01, 01)
+    y = datetime(2012, 02, 01)
+    td = ImprovedTimeDelta(x, y)
+    print td
+    assert str(td) == "0 Years, 1 Months, 0 Days"
+
+    x = datetime(2012, 01, 01)
+    y = datetime(2013, 02, 01)
+    td = ImprovedTimeDelta(x, y)
+    print td
+    assert str(td) == "1 Years, 1 Months, 0 Days"
+
+    x = datetime(2012, 02, 01)
+    y = datetime(2013, 01, 01)
+    td = ImprovedTimeDelta(x, y)
+    print td
+    assert str(td) == "0 Years, 11 Months, 0 Days"
+
+    x = datetime(2012, 01, 01)
+    y = datetime(2013, 01, 01)
+    td = ImprovedTimeDelta(x, y)
+    print td
+    assert str(td) == "1 Years, 0 Months, 0 Days"
+
+    x = datetime(2012, 02, 01)
+    y = datetime(2014, 01, 01)
+    td = ImprovedTimeDelta(x, y)
+    print td
+    assert str(td) == "1 Years, 11 Months, 0 Days"
+
+    x = datetime(2012, 03, 30)
+    y = datetime(2014, 02, 04)
+    td = ImprovedTimeDelta(x, y)
+    print td
+    assert str(td) == "1 Years, 9 Months, 6 Days"
+
+    x = datetime(2012, 03, 06)
+    y = datetime(2014, 03, 04)
+    td = ImprovedTimeDelta(x, y)
+    print td
+    assert str(td) == "1 Years, 10 Months, 30 Days"
+
+    # leap year test
+    # first with not a leap year
+    x = datetime(2013, 02, 06)
+    y = datetime(2013, 04, 07)
+    td = ImprovedTimeDelta(x, y)
+    print td
+    assert str(td) == "0 Years, 1 Months, 30 Days"
+
+    x = datetime(2012, 02, 06)
+    y = datetime(2012, 04, 07)
+    td = ImprovedTimeDelta(x, y)
+    print td
+    assert str(td) == "0 Years, 1 Months, 31 Days"
+
+    td = ImprovedTimeDelta(datetime(2012, 02, 12), datetime.now())
+    print td
